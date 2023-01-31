875ed2cd3e651f2841c4356a0f58bd3b593bd56d
Joel-HackMaster <122944694+Joel-HackMaster@users.noreply.github.com>
2023-01-31 14:37:07 -0500

    Add files via upload

---
+++ EjercicioTema3.java
@@ -0,0 +1,11 @@
+package com.javasesion1.ClaseString;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+        String[] sentece = {"Hola ", "buenos ", "dias"};
+
+        for(int i=0; i<sentece.length; i++){
+            System.out.print(sentece[i]);
+        }
+    }
+}
